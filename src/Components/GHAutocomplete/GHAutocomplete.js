2dcaca04b4c639f57039c16db5bdade1a543b7c4
a.Ghafourian <a.Ghafourian@DESKTOP-63EQ1G8>
2023-09-26 15:03:25 +0330

    added translation tag instead of useTranslationHook

--- src/Components/GHAutocomplete/GHAutocomplete.js
+++ src/Components/GHAutocomplete/GHAutocomplete.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Autocomplete from "@mui/material/Autocomplete"
 import CircularProgress from "@mui/material/CircularProgress"
+import { Translation } from "react-i18next"
 
 const GHAutocomplete = ({
     innerWidth = 300,
@@ -14,44 +15,48 @@ const GHAutocomplete = ({
     const [fontSize, setFontSize] = useState(innerFontSize)
     const [bgColor, setBgColor] = useState(backgroundColor)
     return (
-        <Autocomplete 
-            componentsProps={{
-                paper: {
-                    sx: {
-                        width: { width },
-                        maxWidth: '90vw',
-                        // direction: i18n.dir(),
-                        position: "absolute",
-                        fontSize: { fontSize },
-                        // right: i18n.dir() === "rtl" ? "0" : "unset"
+        <Translation>
+            {(t, { i18n }) => (
+                <Autocomplete
+                    componentsProps={{
+                        paper: {
+                            sx: {
+                                width: { width },
+                                maxWidth: '90vw',
+                                direction: i18n.dir(),
+                                position: "absolute",
+                                fontSize: { fontSize },
+                                right: i18n.dir() === "rtl" ? "0" : "unset"
+                            }
+                        }
+                    }}
+                    sx={
+                        {
+                            direction: i18n.dir(),
+                            position: "relative",
+                            background: { bgColor },
+                            borderRadius: 0,
+                            fontSize: { fontSize }
+                        }
                     }
-                }
-            }}
-            sx={
-                {
-                    // direction: i18n.dir(),
-                    position: "relative",
-                    background: { bgColor },
-                    borderRadius: 0,
-                    fontSize: { fontSize }
-                }
-            }
-            size="small"
-            isOptionEqualToValue={(option, value) => option?.value === value?.value}
-            disableClearable={!allowClear}
-            forcePopupIcon={false}
-            // noOptionsText={t("اطلاعات یافت نشد")}
-            noOptionsText={"اطلاعات یافت نشد"}
-            loading
-            // loadingText={loadingState ? <CircularProgress /> : t("اطلاعات یافت نشد")}
-            loadingText={loadingState ? <CircularProgress /> : "اطلاعات یافت نشد"}
-            renderInput={(params) => (
-                <div ref={params.InputProps.ref}>
-                    <input type="text" {...params.inputProps} className='form-input' />
-                </div>
+                    size="small"
+                    isOptionEqualToValue={(option, value) => option?.value === value?.value}
+                    disableClearable={!allowClear}
+                    forcePopupIcon={false}
+                    noOptionsText={t("اطلاعات یافت نشد")}
+                    // noOptionsText={"اطلاعات یافت نشد"}
+                    loading
+                    loadingText={loadingState ? <CircularProgress /> : t("اطلاعات یافت نشد")}
+                    // loadingText={loadingState ? <CircularProgress /> : "اطلاعات یافت نشد"}
+                    renderInput={(params) => (
+                        <div ref={params.InputProps.ref}>
+                            <input type="text" {...params.inputProps} className='form-input' />
+                        </div>
+                    )}
+                    {...props}
+                />
             )}
-            {...props}
-        />
+        </Translation>
     )
 }
 export default GHAutocomplete
