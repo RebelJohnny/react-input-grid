349443b1b142ad35fa973442ec82eed215a349e5
a.Ghafourian <a.Ghafourian@DESKTOP-63EQ1G8>
2023-09-26 15:26:01 +0330

    changed to HOC for translation

--- src/Components/GHAutocomplete/GHAutocomplete.js
+++ src/Components/GHAutocomplete/GHAutocomplete.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react"
 import Autocomplete from "@mui/material/Autocomplete"
 import CircularProgress from "@mui/material/CircularProgress"
-import { Translation } from "react-i18next"
-
+import { withTranslation } from "react-i18next"
 const GHAutocomplete = ({
+    t,
+    i18n,
     innerWidth = 300,
     innerFontSize = '12px',
     backgroundColor = '#FFFFFF',
@@ -15,48 +16,44 @@ const GHAutocomplete = ({
     const [fontSize, setFontSize] = useState(innerFontSize)
     const [bgColor, setBgColor] = useState(backgroundColor)
     return (
-        <Translation>
-            {(t, { i18n }) => (
-                <Autocomplete
-                    componentsProps={{
-                        paper: {
-                            sx: {
-                                width: { width },
-                                maxWidth: '90vw',
-                                direction: i18n.dir(),
-                                position: "absolute",
-                                fontSize: { fontSize },
-                                right: i18n.dir() === "rtl" ? "0" : "unset"
-                            }
-                        }
-                    }}
-                    sx={
-                        {
-                            direction: i18n.dir(),
-                            position: "relative",
-                            background: { bgColor },
-                            borderRadius: 0,
-                            fontSize: { fontSize }
-                        }
+        <Autocomplete
+            componentsProps={{
+                paper: {
+                    sx: {
+                        width: { width },
+                        maxWidth: '90vw',
+                        direction: i18n.dir(),
+                        position: "absolute",
+                        fontSize: { fontSize },
+                        right: i18n.dir() === "rtl" ? "0" : "unset"
                     }
-                    size="small"
-                    isOptionEqualToValue={(option, value) => option?.value === value?.value}
-                    disableClearable={!allowClear}
-                    forcePopupIcon={false}
-                    noOptionsText={t("اطلاعات یافت نشد")}
-                    // noOptionsText={"اطلاعات یافت نشد"}
-                    loading
-                    loadingText={loadingState ? <CircularProgress /> : t("اطلاعات یافت نشد")}
-                    // loadingText={loadingState ? <CircularProgress /> : "اطلاعات یافت نشد"}
-                    renderInput={(params) => (
-                        <div ref={params.InputProps.ref}>
-                            <input type="text" {...params.inputProps} className='form-input' />
-                        </div>
-                    )}
-                    {...props}
-                />
+                }
+            }}
+            sx={
+                {
+                    direction: i18n.dir(),
+                    position: "relative",
+                    background: { bgColor },
+                    borderRadius: 0,
+                    fontSize: { fontSize }
+                }
+            }
+            size="small"
+            isOptionEqualToValue={(option, value) => option?.value === value?.value}
+            disableClearable={!allowClear}
+            forcePopupIcon={false}
+            noOptionsText={t("اطلاعات یافت نشد")}
+            // noOptionsText={"اطلاعات یافت نشد"}
+            loading
+            loadingText={loadingState ? <CircularProgress /> : t("اطلاعات یافت نشد")}
+            // loadingText={loadingState ? <CircularProgress /> : "اطلاعات یافت نشد"}
+            renderInput={(params) => (
+                <div ref={params.InputProps.ref}>
+                    <input type="text" {...params.inputProps} className='form-input' />
+                </div>
             )}
-        </Translation>
+            {...props}
+        />
     )
 }
-export default GHAutocomplete
+export default withTranslation()(GHAutocomplete)
